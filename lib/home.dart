192756d86b378b94620dcb2e5be6d660b3aab722
Roshan <roshan@pucsd@gmail.com>
2020-12-25 19:43:55 +0530

    asymmetric view added

--- lib/home.dart
+++ lib/home.dart
@@ -1,89 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 import 'package:shrine/model/product.dart';
 import 'package:shrine/model/products_repository.dart';
+import 'package:shrine/supplemental/asymmetric_view.dart';
 
 class Homepage extends StatelessWidget {
-  List<Card> _buildGridCards(BuildContext context) {
-    List<Product> products = ProductsRepository.loadProducts(Category.all);
-
-    if (products == null || products.isEmpty) {
-      return const <Card>[];
-    }
-
-    final ThemeData theme = Theme.of(context);
-    final NumberFormat formatter = NumberFormat.simpleCurrency(
-        locale: Localizations.localeOf(context).toString());
-
-    return products.map((product) {
-      return Card(
-        clipBehavior: Clip.antiAlias,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            AspectRatio(
-              aspectRatio: 18.0 / 11.0,
-              child: Image.asset(
-                product.assetName,
-                package: product.assetPackage,
-              ),
-            ),
-            Expanded(
-                child: Padding(
-              padding: EdgeInsets.fromLTRB(16.0, 12, 16, 8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    product.name,
-                    style: theme.textTheme.headline6,
-                    maxLines: 1,
-                  ),
-                  SizedBox(
-                    height: 8.0,
-                  ),
-                  Text(
-                    // '${product.price}',
-                    formatter.format(product.price),
-                    style: theme.textTheme.subtitle2,
-                    // maxLines: 1,
-                  )
-                ],
-              ),
-            ))
-          ],
-        ),
-      );
-    }).toList();
-  }
-  // static cards are made one by one
-  // List<Card> cards = List.generate(count, (int index) {
-  //   return Card(
-  //     clipBehavior: Clip.antiAlias,
-  //     child: Column(
-  //       crossAxisAlignment: CrossAxisAlignment.start,
-  //       children: <Widget>[
-  //         AspectRatio(
-  //           aspectRatio: 18.0 / 11.0,
-  //           child: Image.asset('assets/diamond.png'),
-  //         ),
-  //         Padding(
-  //           padding: EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
-  //           child: Column(
-  //             crossAxisAlignment: CrossAxisAlignment.start,
-  //             children: <Widget>[
-  //               Text('Title'),
-  //               SizedBox(height: 8.0),
-  //               Text('Secondary Text'),
-  //             ],
-  //           ),
-  //         ),
-  //       ],
-  //     ),
-  //   );
-  // });
-  //return cards;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -109,12 +29,8 @@ class Homepage extends StatelessWidget {
           ),
         ],
       ),
-      body: GridView.count(
-        crossAxisCount: 2,
-        padding: EdgeInsets.all(16.0),
-        childAspectRatio: 8.0 / 9.0,
-        // TODO: Build a grid of cards (102)
-        children: _buildGridCards(context),
+      body: AsymmetricView(
+        products: ProductsRepository.loadProducts(Category.all),
       ),
       resizeToAvoidBottomInset: false,
     );
